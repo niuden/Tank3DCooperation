151664d02d8916273a4ae4c189cd4701ddf23a6e
niuden <1411432213@qq.com>
2021-07-21 18:48:38 +0800

    Cooperation 2021-7-21 18:48

---
+++ Assets/TankBack/Scripts/Demo/Player/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed;
+    public float speed;
+
+    public GameObject fire;
+    public GameObject bullet;
+
+    private float horizontal;
+    private float vertical;
+
+    private bool isAttack;
+
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        if (Input.GetButtonDown("Fire"))
+        {
+            isAttack = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Turn();
+        Attack();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = transform.forward * vertical * speed * Time.deltaTime;
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+    }
+    
+    private void Turn()
+    {
+        float turn = horizontal * turnSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+    }
+    
+    private void Attack()
+    {
+        if (isAttack)
+        {
+            Instantiate(bullet, fire.transform.position, fire.transform.rotation);
+            isAttack = false;
+        }
+    }
+
+}
